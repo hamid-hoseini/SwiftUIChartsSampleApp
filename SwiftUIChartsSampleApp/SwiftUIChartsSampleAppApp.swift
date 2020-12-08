d59de32551f46eaf7245a3e480bfba5cccc1fa8f
Hamid <mail.hamid.hoseini@gmail.com>
2020-12-07 22:28:40 -0800

    Initial Commit

---
+++ SwiftUIChartsSampleApp/SwiftUIChartsSampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIChartsSampleAppApp.swift
+//  SwiftUIChartsSampleApp
+//
+//  Created by Hamid Hoseini on 12/7/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIChartsSampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
