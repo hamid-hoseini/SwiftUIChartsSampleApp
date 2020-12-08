15ae5d95d81e81d9472488388a2696f19f943491
Hamid <mail.hamid.hoseini@gmail.com>
2020-12-07 23:00:41 -0800

    first commit

--- SwiftUIChartsSampleApp/ContentView.swift
+++ SwiftUIChartsSampleApp/ContentView.swift
@@ -5,12 +5,35 @@
 //  Created by Hamid Hoseini on 12/7/20.
 //
 
+import SwiftUICharts
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            LineView(data: [12, 22, 3, 6, 16, 9, 2], title: "Line Chart")
+            Spacer()
+            
+            // Line Chart
+//            LineChartView(data: [12, 22, 3, 6, 16, 9, 2], title: "Line Chart")
+//            Spacer()
+//
+//            // Bar Chart
+//            BarChartView(data: ChartData(values: [
+//                ("Jan", 12),
+//                ("Feb", 3),
+//                ("Mar", 4),
+//                ("Apr", 10),
+//                ("May", 2),
+//                ("Jun", 9)
+//            ]),
+//            title: "Bar Chart")
+//            Spacer()
+//
+//            // Pie Chart
+//            PieChartView(data: [22, 17,32, 99, 78, 64], title: "Pie Chart")
+//            Spacer()
+        }
     }
 }
 
